df00a8e1054b1fb7c7ad778e5efa933af7cf9e3d
Peam <peammy2010@hotmail.com>
2023-04-17 19:41:50 +0700

    feat: ✨ authentication route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import * as cors from "cors";
+import cors from "cors";
+import { routes } from "./routes/index.js";
+import { oauthControllers } from "./controllers/oauth/index.js";
 dotenv.config();
 const app = express();
 
@@ -14,6 +16,14 @@ app.use(
   })
 );
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.use("/auth", routes.authRoute);
+app.get("/courseville/access_token", oauthControllers.token);
 
 app.listen(port, () => {
   console.log(`Server run at http://localhost:${port}`);
