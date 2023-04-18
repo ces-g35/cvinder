662f13a9a39bbe19acb4113d64d6a6feb06425b2
Peam <peammy2010@hotmail.com>
2023-04-18 13:08:53 +0700

    feat: ✨ lazy import

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,23 +5,19 @@ const render = (root, data) => {
 };
 
 const router = async () => {
-  switch (window.location.pathname) {
-    case "/":
-      const index = await import("./components/index.js");
-      render(rootElement, index.default);
-      break;
-    case "/interests":
-      const interests = await import("./components/interests.js");
-      render(rootElement, interests.default);
-      break;
-    case "/add-photos":
-      const photodefault = await import("./components/photo.js");
-      render(rootElement, photodefault);
-      break;
-    default:
-      const error404 = await import("./components/404.js");
-      render(rootElement, error404.default);
-      break;
+  const path = window.location.pathname;
+  if (path.startsWith("/interests")) {
+    const interests = await import("./components/interests.js");
+    render(rootElement, interests.default);
+  } else if (path.startsWith("/add-photos")) {
+    const photodefault = await import("./components/photo.js");
+    render(rootElement, photodefault);
+  } else if (path.startsWith("/")) {
+    const index = await import("./components/index.js");
+    render(rootElement, index.default);
+  } else {
+    const error404 = await import("./components/404.js");
+    render(rootElement, error404.default);
   }
 };
 
