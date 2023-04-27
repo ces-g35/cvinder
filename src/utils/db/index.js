a802dcf325f0c2985e4c38c49a7f0cf9844c2df1
msp5382 <meen.sankaew@gmail.com>
2023-04-27 19:23:00 +0700

    fix: duplicated import from merging conflict

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -1,9 +1,6 @@
 import { v4 as uuid } from "uuid";
 import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 import {
-  QueryCommand,
-} from "@aws-sdk/client-dynamodb";
-import {
   PutCommand,
   DeleteCommand,
   ScanCommand,
