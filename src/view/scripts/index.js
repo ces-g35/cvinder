5b106936d0d41b8b0e9b75849525394d51a2a704
msp5382 <meen.sankaew@gmail.com>
2023-05-02 12:41:57 +0700

    fix: mobile safe area

--- src/view/scripts/index.js
+++ src/view/scripts/index.js
@@ -1,3 +1,8 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 const COOKIE_TTL = 1209600;
 
 window.useGuard = (isSet = false) => {
